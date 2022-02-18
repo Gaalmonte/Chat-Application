dc12c1b3fcf250f41c1e6f1a4f7c7f8cacba6d42
german Almonte <german.a.almonte@gmail.com>
2022-02-18 15:15:37 -0500

    set up firebase

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 In this project I will be practicing and exercising my React skills while also learning Firebase. 
 
 ## Goal
-By the end of this project I will deploy it and include the link on the bottom of this readme. You should be able to chat with anyone that joins. There will be rules in order to prevent the chat room from shutting down due to spam, or to mute someone with vulgar language.
+* By the end of this project I will deploy it and include the link on the bottom of this readme.
+* You should be able to chat with anyone that joins. 
+* There will be rules in order to prevent the chat room from shutting down due to spam, or to mute someone with vulgar language.
+* Learn how to authenticate users.
+* Learn to work with Realtime data streams
--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// imported a couple of hooks to make it easier to work with firebase and react 
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {useCollectionData} from 'react-firebase-hooks'
+
+firebase.initializeApp({
+  // config
+  apiKey: "AIzaSyDEMGCKqGpt7QLJwCz8cyFIfF-BDSvuUtI",
+  authDomain: "superchat-d42b8.firebaseapp.com",
+  projectId: "superchat-d42b8",
+  storageBucket: "superchat-d42b8.appspot.com",
+  messagingSenderId: "880623964700",
+  appId: "1:880623964700:web:a8d233b198c48218c945d9",
+  measurementId: "G-85Z3KZL5TZ"
+})
+
+// Global variables
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
